033abd151c53e60a6619288ddf5041bd36347135
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-06-05 16:16:28 +0200

    folderStructure

---
+++ script/code .js
@@ -0,0 +1,13 @@
+let addToDoButton = document.getElementById('#add');
+let containerArea = document.getElementById('.conatiner');
+let inputField = document.getElementById('#input');
+
+
+
+addToDoButton.addEventListener('click', function (){
+
+    this.innerHTML()
+
+});
+
+
